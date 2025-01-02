a54fe101e953e8698ec8002a5feeb2653b61d3a7
kklecho <krzysztof.lechowski@gmail.com>
2025-01-02 20:52:29 +0000

    fix coutner overrite

--- update_counter.sh
+++ update_counter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -c 'import random; print(random.randint(1,3))' >> counter.txt
+python -c 'import random; print(random.randint(1,3))' > counter.txt
 git config --global user.name 'Krzysztof Lechowski'
 git config --global user.email 'krzysztof.lechowski@gmail.com'
 git add counter.txt
