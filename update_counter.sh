23d1bb1dc54a014d62987d9e7e104a5d5d69cab7
kklecho <krzysztof.lechowski@gmail.com>
2025-01-02 00:14:24 +0000

    Add counter updater gh action

---
+++ update_counter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -c 'import random; print(random.randint(1,3))' >> counter.txt
+git config --global user.name 'Krzysztof Lechowski'
+git config --global user.email 'krzysztof.lechowski@gmail.com'
+git add counter.txt
+git commit -m "updated counter"
+git push
