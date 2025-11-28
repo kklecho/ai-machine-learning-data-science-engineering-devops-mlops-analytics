2ad769bbdb9a4ec7ea950987abcdef515e96f4fc
kklecho <krzysztof.lechowski@gmail.com>
2025-11-28 00:36:44 +0000

    improve labeling consistency

--- update_counter.sh
+++ update_counter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python -c 'import random; print(random.randint(1,3))' > counter.txt
+python -c 'import random; print(random.randint(1,3))' > analysis.csv
 git config --global user.name 'Krzysztof Lechowski'
 git config --global user.email 'krzysztof.lechowski@gmail.com'
-git add counter.txt
-git commit -m "updated counter"
+git add analysis.csv
+git commit -m "Analysis for digital twin against Bayesian RAG ML GPT alternative hypothesis"
 git push
